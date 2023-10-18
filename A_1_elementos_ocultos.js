9feecda9cc0d3e21999b00f211ecaebb19082b79
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-18 09:56:21 +0200

    Mostrar elementos ocultos

---
+++ A_1_elementos_ocultos.js
@@ -0,0 +1,10 @@
+// Can show hidden HTML elements.
+
+<script language="JavaScript">
+
+  <p id="demo1" style="display:none">Hello JavaScript!</p>
+
+  <button type="button" onclick="document.getElementById('demo1').style.display='block'">Click Me!</button>
+
+</script>
+         
