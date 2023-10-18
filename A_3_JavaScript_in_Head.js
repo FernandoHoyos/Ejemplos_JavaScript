b8b9a3106a78732c5cef1b9be341fb3f2f16a126
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-18 10:04:13 +0200

    JavaScript dentro de la cabecera, llamado al body con una funcion

---
+++ A_3_JavaScript_in_Head.js
@@ -0,0 +1,13 @@
+// Demo JavaScript in Head
+
+  <script>
+    function myFunction1() {
+      document.getElementById("demo4").innerHTML = "Paragraph changed.";
+    }
+  </script>
+
+//Body
+
+  <p id="demo4">A Paragraph</p>
+
+  <button type="button" onclick="myFunction1()">Try it</button>
