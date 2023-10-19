49021c8c2f4c070df16310c7273d4eccdd1a5cf9
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-19 21:08:16 +0200

    JavaScript dentro del cuerpo HTML (Body)

---
+++ A_4_JavaScript_in_Body.js
@@ -0,0 +1,10 @@
+//Demo JavaScript in Body
+
+  <p id="demo4">A Paragraph</p>
+  <button type="button" onclick="myFunction2()">Try it</button>
+
+ <script>
+    function myFunction2() {
+      document.getElementById("demo4").innerHTML = "Paragraph changed.";
+    }
+ </script>
