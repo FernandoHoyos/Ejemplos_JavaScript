a9f7b5414e5fa6fd188728c84351a1fc2ac5019c
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-19 21:14:40 +0200

    JavaScript Externo por URL

---
+++ A_6_External_JavaScript_URL.js
@@ -0,0 +1,8 @@
+//External JavaScript URL
+  <p id="demo6">A Paragraph.</p>
+  <button type="button" onclick="myFunction()">Click Me</button>
+
+  <p>This example uses a full web URL to link to "myScript.js".</p>
+  <p>(myFunction is stored in "myScript.js")</p>
+
+  <script src="https://www.w3schools.com/js/myScript.js"></script>
