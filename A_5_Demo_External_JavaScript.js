9f7de404d91d3a72931f245012c102228ddf1aea
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-19 21:12:31 +0200

    JavaScript Externo del cuerpo HTML (Body)

---
+++ A_5_Demo_External_JavaScript.js
@@ -0,0 +1,10 @@
+//Demo External JavaScript
+
+  <p id="demo6">A Paragraph.</p>
+  <button type="button" onclick="myFunction3()">Try it</button>
+
+  <p>This example links to "myScript6.js".</p>
+
+  <p>(myFunction is stored in "myScript6.js")</p>
+
+  <script src="script/myScript6.js"></script>
