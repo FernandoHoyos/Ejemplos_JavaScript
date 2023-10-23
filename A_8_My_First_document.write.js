3708705ae98a838fe0f6a28e2ea3928200c95ddb
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-23 18:45:09 +0200

    Mi primer document.write sumando +

---
+++ A_8_My_First_document.write.js
@@ -0,0 +1,8 @@
+//My First document.write
+  <p>Nunca llame a document.write después de que el documento haya terminado de cargarse. Sobrescribirá todo el
+    documento.</p>
+  <p>Se usa para fines de prueba</p>
+  <script>
+    document.write("La suma es ", 5 + 6);
+  </script>
+         
