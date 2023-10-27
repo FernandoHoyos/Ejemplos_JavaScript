dc39d360ccd1d61ddf967b43cdd38228f76969f2
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-27 19:55:03 +0200

    Activar depuración console.log(...)

---
+++ A_11_Activate_Debugging.js
@@ -0,0 +1,7 @@
+  //Activate Debugging/Activar depuración
+  //F12 on your keyboard will activate debugging.
+  //Then select "Console" in the debugger menu.
+  //Then click Run again.
+  <script>
+    console.log("Debugging");
+  </script>
