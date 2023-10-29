32368fd991f49e691fc09f0beb900efffb8c163b
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-29 17:14:44 +0100

    Lista de sentencias para ser ejecutadas

---
+++ A_12_Lista_de_sentencias_para_ser_ejecutadas.js
@@ -0,0 +1,12 @@
+
+//Declaraciones JavaScript (Ejecutadas por una computadora.)
+ //A <b>JavaScript program</b> is a list of <b>statements</b> to be executed by a computer.
+  //Un programa JavaScript es una lista de sentencias para ser ejecutadas por una computadora.
+  <p id="demo8"></p>
+  <script>
+    let x, y, z;  // Statement 1
+    x = 5;        // Statement 2
+    y = 6;        // Statement 3
+    z = x + y;    // Statement 4
+    document.getElementById("demo8").innerHTML = "The value of z is " + z + ".";  
+  </script>
