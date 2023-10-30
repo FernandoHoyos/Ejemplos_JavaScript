557c9c02591129562edc9f83a86a538b96790a91
FernandoHoyos <technologicallprocess@gmail.com>
2023-10-30 16:58:44 +0100

    Sentencias de JavaScript son ejecutadas por el navegador

---
+++ A_13_sentencias_JavaScript.js
@@ -0,0 +1,7 @@
+//Declaraciones JavaScript (Ejecutadas por el navegador.)//
+//In HTML, JavaScript statements are executed by the browser.//
+//En HTML, las sentencias de JavaScript son ejecutadas por el navegador.//
+  <p id="demo9"></p>
+  <script>
+    document.getElementById("demo9").innerHTML = "Hola muñequita.";
+  </script>
