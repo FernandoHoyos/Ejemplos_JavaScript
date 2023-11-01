02e8d0f35856b5d46904388a0000bae751d38656
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-01 21:17:59 +0100

    Las declaracion y suma de variables y mostradas en un innerHTML

---
+++ A_14_Declaraciones.js
@@ -0,0 +1,12 @@
+//JavaScript Statements
+
+//JavaScript statements are separated by semicolons.
+ //Las declaraciones de JavaScript están separadas por punto y coma.
+  <p id="demo10"></p>
+  <script>
+    let a1, b1, c1;
+    a1 = 19;
+    b1 = 17;
+    c1 = a1 + b1;
+    document.getElementById("demo10").innerHTML = c1;
+  </script>
