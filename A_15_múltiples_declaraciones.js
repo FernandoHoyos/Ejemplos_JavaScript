28592ac40ebbc002de3b7d42b9a546d3b2582bd8
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-02 16:04:46 +0100

    Múltiples declaraciones en una línea

---
+++ A_15_múltiples_declaraciones.js
@@ -0,0 +1,9 @@
+//JavaScript Statements
+//Multiple statements on one line are allowed.
+//Se permiten múltiples declaraciones en una línea.
+  <p id="demo11"></p>
+  <script>
+    let a2, b2, c2;
+    a2 = 19; b2 = 17; c2 = a2 + b2;
+    document.getElementById("demo11").innerHTML = c2;
+  </script>
