09f876bef8133382a7a28f1464f02bf4de46062d
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-03 17:19:09 +0100

    El mejor lugar para dividir una línea de código es después de un operador o una coma

---
+++ A_16_El_mejor_lugar_para_dividir_una_linea.js
@@ -0,0 +1,9 @@
+//JavaScript Statements
+   //The best place to break a code line is after an operator or a comma.
+  //El mejor lugar para dividir una línea de código es después de un operador o una coma.
+  <p id="demo12"></p>
+  <script>
+    document.getElementById("demo12").innerHTML =
+      "Hello Dolly!";
+  </script>
+  <hr>
