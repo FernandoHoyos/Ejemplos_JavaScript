a49e00b05ab48b54fda0e1a614d08f633f1f1ccb
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-04 13:18:43 +0100

    JavaScript code blocks are written between { and }, Los bloques de código JavaScript se escriben entre { y }

---
+++ A_17_JavaScript_code_blocks_between_brackets.js
@@ -0,0 +1,12 @@
+//JavaScript Statements (function)
+//JavaScript code blocks are written between { and }
+//Los bloques de código JavaScript se escriben entre { y }
+  <button type="button" onclick="myFunction()">Click Me!</button>
+  <p id="demo13"></p>
+  <p id="demo14"></p>
+  <script>
+    function myFunction() {
+      document.getElementById("demo13").innerHTML = "Hello Dolly!";
+      document.getElementById("demo14").innerHTML = "How are you?";
+    }
+  </script>
