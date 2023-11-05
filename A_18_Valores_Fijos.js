ee359417b33cfbd09c06e63e7df974cdde53781d
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-05 16:12:03 +0100

    Number can be written with or without decimals/El número se puede escribir con o sin decimales.

---
+++ A_18_Valores_Fijos.js
@@ -0,0 +1,8 @@
+//Valores de JavaScript
+//Valores fijos
+//Number can be written with or without decimals.
+<p>El número se puede escribir con o sin decimales.</p>
+  <p id="demo15"></p>
+  <script>
+    document.getElementById("demo15").innerHTML = 10.50;
+  </script>
