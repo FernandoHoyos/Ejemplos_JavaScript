05a8afa8d1d83e2e440ce96b8da63f759c69c2f5
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-06 22:28:57 +0100

    C:/Program Files/Git/Strings can be written with double or single quotes/Las cadenas se pueden escribir con comillas dobles o simples.

---
+++ A_19_Valor_string.js
@@ -0,0 +1,7 @@
+//JavaScript Strings
+//Strings can be written with double or single quotes
+//Las cadenas se pueden escribir con comillas dobles o simples
+  <p id="demo16"></p>
+  <script>
+    document.getElementById("demo16").innerHTML = 'John Doe';
+  </script>
