d2305c2737ed03b475e4e54d6ef6e2ffc5dfe7d0
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-09 08:59:23 +0100

    Declare a variable and assign it a value/Declarar una variable y asígnele un valor.

---
+++ A_20_Declarar_variable.js
@@ -0,0 +1,16 @@
+//JavaScript Variables
+/*<code>
+  lef
+  const
+  var
+  <code>
+*/
+//Declarar una variable de JavaScript
+//Create a variable, assign a value to it, and display it:
+//Cree una variable, asígnele un valor y muéstrela:
+
+<p id="demo17"></p>
+<script>
+  let carName1 = "Volvo";
+  document.getElementById("demo17").innerHTML = carName1;
+</script>
