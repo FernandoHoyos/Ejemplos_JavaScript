e64e1d736f14c00c7f81020bc3a7ffa3416007e5
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-10 09:30:20 +0100

    A statement with many variables/Una declaración con Muchas variables.

---
+++ 21_Una_declaracion_Muchas_variables.js
@@ -0,0 +1,12 @@
+//Una declaración, muchas variables
+//You can declare many variables in one statement.
+//Puede declarar muchas variables en una sola declaración.
+<p id="demo18"></p>
+<p id="demo19"></p>
+<p id="demo20"></p>
+<script>
+  let person = "John Doe", carName2 = "Volvo", price = 200;
+  document.getElementById("demo18").innerHTML = person;
+  document.getElementById("demo19").innerHTML = carName2;
+  document.getElementById("demo20").innerHTML = price;
+</script>
