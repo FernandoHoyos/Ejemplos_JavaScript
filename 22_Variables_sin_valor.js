f756dfbe7419c8cbb3963c85af74e27b1f8e2b58
FernandoHoyos <technologicallprocess@gmail.com>
2023-11-11 16:52:25 +0100

    A variable without a value has the value of / Una variable sin valor tiene el valor de: undefined

---
+++ 22_Variables_sin_valor.js
@@ -0,0 +1,9 @@
+//Variables sin valor
+//A variable without a value has the value of/Una variable sin valor tiene el valor de:
+//undefined
+
+<p id="demo21"></p>
+<script>
+  let carName3;
+  document.getElementById("demo21").innerHTML = carName3;
+</script>
