b46cb93c3be3cfc95813d601bfdadfd2adb77579
FernandoHoyos <technologicallprocess@gmail.com>
2023-12-23 19:38:33 +0100

    Numerical variables.

---
+++ 23_Variables_numéricas.js
@@ -0,0 +1,9 @@
+//Variables numericas
+//The result of adding 5 + 2 + 3 is:</p>
+//El resultado de sumar 5 + 2 + 3 es:</p>
+<p id="demo22"></p>
+
+<script>
+  let x1 = 5 + 2 + 3;
+  document.getElementById("demo22").innerHTML = x1;
+</script>
