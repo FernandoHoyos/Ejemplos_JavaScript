db42ade7b2e473fae450c427316b3009ae40fbb8
FernandoHoyos <technologicallprocess@gmail.com>
2023-12-24 18:16:49 +0100

    String variables.

---
+++ 24_Variables_strings.js
@@ -0,0 +1,10 @@
+//Variables strings
+
+//The result of adding "John" + " " + "Doe" is:
+//El resultado de sumar "John" + " " + "Doe" es:
+<p id="demo23"></p>
+
+<script>
+  let x2 = "John" + " " + "Doe";
+  document.getElementById("demo23").innerHTML = x2;
+</script>
