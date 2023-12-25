3848ae3a2c7027e106723745405ecbbd46aad2bc
FernandoHoyos <technologicallprocess@gmail.com>
2023-12-25 18:07:37 +0100

    Dollar sign $

---
+++ 25_Signo_de_dólar_$.js
@@ -0,0 +1,12 @@
+//Signo de dólar $
+
+//The dollar sign is treated as a letter in JavaScript names.
+//El signo de dólar se trata como una letra en los nombres de JavaScript.
+
+<p id="demo24"></p>
+
+<script>
+  let $$$ = 2;
+  let $myMoney = 5;
+  document.getElementById("demo24").innerHTML = $$$ + $myMoney;
+</script>
