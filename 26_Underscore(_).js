79e446877be71391fc78fa2773b07a677a2a0a8d
FernandoHoyos <technologicallprocess@gmail.com>
2023-12-26 16:40:21 +0100

    Underscore _

---
+++ 26_Underscore(_).js
@@ -0,0 +1,14 @@
+//Underscore (_)
+
+//The underscore is treated as a letter in JavaScript names.
+//El guión bajo se trata como una letra en los nombres de JavaScript.
+  <code>
+  let _x = 2;<br>
+  let _100 = 5;
+  </code>
+  <p id="demo25"></p>
+  <script>
+    let _x = 2;
+    let _100 = 5;
+    document.getElementById("demo25").innerHTML = _x + _100;
+  </script>
