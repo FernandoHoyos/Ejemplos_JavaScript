0042bdebf1b8b163bc00b9ab407ba72f64d541f0
FernandoHoyos <technologicallprocess@gmail.com>
2023-12-29 16:29:07 +0100

    Redeclaring var

---
+++ 29_Redeclarar_Var.js
@@ -0,0 +1,11 @@
+//var
+//Redeclaring a JavaScript variable with <b>var</b> is allowed anywhere in a program:
+//Se permite redeclarar una variable JavaScript con var en cualquier parte de un programa:
+  <p id="demo28"></p>
+  <script>
+    var x5 = 2;
+    // Now x is 2
+    var x5 = 3;
+    // Now x is 3
+    document.getElementById("demo28").innerHTML = x5;
+  </script>
